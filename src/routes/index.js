bbdc5f136a026c8195abaaa1c896b9797410d58e
Võ Nguyên Chương <92419145+VNCChuong@users.noreply.github.com>
2024-09-06 02:18:04 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,7 @@ const ApplyRouter = require('./ApplyRouter')
 const RecruiterRouter = require('./RecruiterRouter')
 const VerifyRouter = require('./VerifyRouter')
 
-const Hello = async (req, res) => {
-    return res.status(200).json({
-        status: 'OK',
-        message: 'Hi Frontend. Backend already!'
-    })
-
-}
 const routes = (app) => {
-    app.use('/', Hello)
     app.use('/api/user', UserRouter)
     app.use('/api/jobpost', JobPostRouter)
     app.use('/api/savejob', SaveJobRouter)
