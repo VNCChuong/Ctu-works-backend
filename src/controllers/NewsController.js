86443d743062ef23a6fd871eaa2278349f7b84bd
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2024-10-24 22:55:04 +0700

    update news

--- src/controllers/NewsController.js
+++ src/controllers/NewsController.js
@@ -1,82 +1,92 @@
-const NewsService = require('../services/NewsService')
+const NewsService = require("../services/NewsService");
 
 const createNews = async (req, res) => {
-    try {
-        const { title, summary, content } = req.body
-        if (!title || !summary || !content) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await NewsService.createNews(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const { title, summary, content } = req.body;
+    if (!title || !summary || !content) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await NewsService.createNews(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
 const updateNews = async (req, res) => {
-    try {
-        const { id, data } = req.body
-        if (!id || !data) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await NewsService.updateNews(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const { id, data } = req.body;
+    if (!id || !data) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await NewsService.updateNews(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const deleteNews = async (req, res) => {
-    try {
-        const id = req.params.id
-        if (!id) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The id is required'
-            })
-        }
-        const response = await NewsService.deleteNews(id)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The id is required",
+      });
     }
-}
-
+    const response = await NewsService.deleteNews(id);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getDetailNews = async (req, res) => {
-    try {
-        const id = req.params.id
-        if (!id) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The id is required'
-            })
-        }
-        const response = await NewsService.getDetailNews(id)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The id is required",
+      });
     }
-}
+    const response = await NewsService.getDetailNews(id);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
+const getAllNews = async (req, res) => {
+  try {
+    const response = await NewsService.getAllNews();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 module.exports = {
-    createNews,
-    updateNews,
-    deleteNews,
-    getDetailNews,
-
-}
+  createNews,
+  updateNews,
+  deleteNews,
+  getDetailNews,
+  getAllNews,
+};
--- src/routes/NewsRouter.js
+++ src/routes/NewsRouter.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const router = express.Router()
-const NewsController = require('../controllers/NewsController');
+const router = express.Router();
+const NewsController = require("../controllers/NewsController");
 
-router.post('/create',  NewsController.createNews)
+router.post("/create", NewsController.createNews);
 
-router.put('/update/:id',  NewsController.deleteNews)
+router.put("/update/:id", NewsController.deleteNews);
 
-router.delete('/delete/:id',  NewsController.deleteNews)
+router.delete("/delete/:id", NewsController.deleteNews);
 
-router.get('/get-detail-news/:id', NewsController.getDetailNews)
+router.get("/get-detail-news/:id", NewsController.getDetailNews);
 
-module.exports = router
+router.get("/get-all-news", NewsController.getAllNews);
+
+module.exports = router;
